b391a79c1fd74f3324f43e39d4cc27e17b941225
Falase Femi <femifalase228@gmail.com>
2023-06-02 23:18:31 +0100

    data called from sanity

--- src/client.ts
+++ src/client.ts
@@ -1,11 +1,15 @@
-import sanityClient from "@sanity/client"
+import {createClient} from "@sanity/client"
 import imageUrlBuilder from "@sanity/image-url"
 
-export const client = sanityClient({
-    projectId: "ugcsob82",
-    dataset: "",
-    apiVersion: "",
-    useCdn: "",
-    token: ""
+export const client = createClient({
+    projectId: import.meta.env.VITE_APP_SANITY_PROJECT_ID,
+    dataset: "production",
+    apiVersion: "2023-06-02",
+    useCdn: true,
+    token: import.meta.env.VITE_APP_SANITY_TOKEN
 })
+
+const builder = imageUrlBuilder(client);
+
+export const urlFor = (source: any) => builder.image(source)
 
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,12 +1,55 @@
+import { useQuery } from "@tanstack/react-query";
+import { client, urlFor } from "../client";
+
+type Project = {
+    description: string;
+    githubLink: string;
+    image: {
+        _type: 'image';
+        asset: any; // You can specify the type for the `asset` property if you have the information
+    };
+    liveSite: string;
+    title: string;
+    _createdAt?: string;
+    _id: string;
+    _rev?: string;
+    _type?: 'project';
+    _updatedAt?: string;
+};
+
+const fetchProjects = async () => {
+    const projects = await client.fetch(`*[_type == "project"]`);
+    return projects;
+};
+
 const Projects = () => {
+    const { data: projects } = useQuery<Project[], Error>(["projects"], fetchProjects);
+    console.log(projects);
+
     return (
         <section className="py-5 md:py-10 mt-5 md:mt-10">
             <div className="text-center">
                 <p className="font-general-medium text-2xl sm:text-4xl mb-1 text-ternary-dark dark:text-ternary-light">Projects portfolio</p>
             </div>
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-6 md:gap-10"></div>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-6 md:gap-10">
+                {projects && projects.map((project) => (
+                    <div key={project._id} className="border border-gray-200 rounded-md p-4">
+                        {/* <img src={urlFor(project.image.asset.)} alt={project.title} className="w-full h-auto mb-4" /> */}
+                        <h2 className="font-medium text-lg mb-2">{project.title}</h2>
+                        <p className="text-gray-500 mb-2">{project.description}</p>
+                        <div className="flex justify-between">
+                            <a href={project.liveSite} target="_blank" rel="noopener noreferrer" className="text-primary hover:text-primary-dark">
+                                Live Site
+                            </a>
+                            <a href={project.githubLink} target="_blank" rel="noopener noreferrer" className="text-primary hover:text-primary-dark">
+                                GitHub
+                            </a>
+                        </div>
+                    </div>
+                ))}
+            </div>
         </section>
-    )
-}
+    );
+};
 
-export default Projects
+export default Projects;
